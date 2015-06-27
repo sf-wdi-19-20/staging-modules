cd6592ee0ccdbc6a641b2984ea202a156e572e02
cameronjacoby <cameron.jacoby@gmail.com>
2015-06-27 08:22:39 -0700

    finish tic tac toe solution

--- w1_tic_tac_toe_solution/ticTacToe.js
+++ w1_tic_tac_toe_solution/ticTacToe.js
@@ -1,27 +1,19 @@
+
 // wait for the DOM to finish loading
 window.addEventListener('DOMContentLoaded', function() {
 
-  // Grab all the necessary elements
-  var board = document.querySelector('#board');
-  var boxes = document.querySelectorAll('.box');
+  // select all the necessary elements
+  var board = document.querySelector('#board'),
+      boxes = document.querySelectorAll('.box'),
+      reset = document.querySelector('#reset');
+  
+  // player X always goes first
   var turn = "X";
-  
-  // var squareBorders = document.querySelectorAll('.square');
-  // var reset = document.querySelector('.reset');
-  // var playerX = document.querySelector('#player_x');
-  // var playerO = document.querySelector('#player_o');
-  // var playCount = 0;
-  // var xConsecWins = 0;
-  // var oConsecWins = 0;
 
-  // reset the turn indicator
-  // displayTurn();
-
-  // Listen for clicks on squares
-  // Track variable 'turn' for next move and end of game
-  // Disallow repeated moves on the same square
-  // Disallow moves after the game has been won
+  // keep track of moves count
+  var moves = 0;
   
+  // helper function to change turn based on current turn
   var changeTurn = function() {
     if (turn === "X") {
       turn = "O";
@@ -30,159 +22,96 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   };
 
+  // helper function to reset board
+  var resetBoard = function() {
+    for (var i = 0; i < boxes.length; i += 1) {
+      boxes[i].innerText = "";
+      
+      // remove `X` or `O` class
+      boxes[i].className = "col-xs-4 box";
+    }
+
+    // player X always goes first
+    turn = "X";
+    
+    // reset moves count
+    moves = 0;
+  };
+
+  // helper function to check for wins in three boxes
+  var allThree = function(player, box1, box2, box3) {
+    return (box1.innerText === player) && (box2.innerText === player) && (box3.innerText === player);
+  };
+
+  // check for wins across both diagonals
+  var winsDiagonal = function(player) {
+    return allThree(player, boxes[0], boxes[4], boxes[8]) ||
+           allThree(player, boxes[2], boxes[4], boxes[6]);
+  };
+
+  // check for wins down all columns
+  var winsColumn = function(player) {
+    return allThree(player, boxes[0], boxes[3], boxes[6]) ||
+           allThree(player, boxes[1], boxes[4], boxes[7]) ||
+           allThree(player, boxes[2], boxes[5], boxes[8]);
+  };
+
+  // check for wins across all rows
+  var winsRow = function(player) {
+    return allThree(player, boxes[0], boxes[1], boxes[2]) ||
+           allThree(player, boxes[3], boxes[4], boxes[5]) ||
+           allThree(player, boxes[6], boxes[7], boxes[8]);
+  };
+
+  // player is winner if wins row, column, or diagonal
+  var winnerIs = function(player) {
+    return winsRow(player) || winsColumn(player) || winsDiagonal(player);
+  };
+
+  // helper function to check for winner
+  var getWinner = function() {
+    if (winnerIs("X")) {
+      return "X";
+    }
+    else if (winnerIs("O")) {
+      return "O";
+    }
+    else {
+      return null;
+    }
+  };
+
+  // listen for clicks on boxes to play the game
   for (var i = 0; i < boxes.length; i += 1) {
     boxes[i].addEventListener('click', function() {
+      
+      // only allow move if box is blank
       if (this.innerText === "") {
         this.innerText = turn;
         this.className += " " + turn;
-        changeTurn();
-        console.log(turn);
+        moves += 1;
+        
+        // check for a winner if 5 or more moves have been played
+        if (moves >= 5) {
+          var winner = getWinner();
+      
+          // if there is a winner, alert the winner and reset the game
+          if (winner) {
+            alert("Player " + winner + " won!");
+            resetBoard();
+          } else {
+            changeTurn();
+          }
+        } else {
+          changeTurn();
+        }
       }
-      // if (event.target.innerHTML === '' && turn !== 'game_over') {  // square hasn't been played
-      //   setSquare(event.target);
-      //   checkGameStatus();
-      // }
-      // else if (turn === 'game_over') {
-      //   alert("The game is over!  Click 'reset' to play again.");
-      // }
-      // else {
-      //   alert('That square has already been played!');
-      // }
     });
   }
 
-  // Reset the board
-  // reset.addEventListener('click', function () {
-  //   for (var i = 0; i < squares.length; i++) {
-  //     squares[i].innerHTML = '';
-  //     squares[i].setAttribute('class', 'blank');
-  //     // reset any highlighted winning squares
-  //     squareBorders[i].setAttribute('class', 'square');
-  //   }
-  //   // X always goes first
-  //   turn = 'X';
-  //   // set the turn indicator
-  //   displayTurn();
-  //   // keep track of how many moves have been made
-  //   // max 9 before a win or tie
-  //   playCount = 0;
-  // });
-
-
-  // square is a p element
-  // function setSquare(square) {
-  //   square.innerHTML = turn;  // turn = X or O
-  //   square.setAttribute('class', turn);
-  //   turn === 'X' ? turn = 'O' : turn = 'X';
-  // }
-
-  // function checkGameStatus() {
-  //   var winner;  // a string to indicate the winner and winning squares, e.g. 'X012'
-  //   if (winner = getWinner()) {
-  //     gameOver(winner);
-  //   }
-  //   else if (++playCount === 9)
-  //     gameOver('tie');
-  //   // game still going, switch turn
-  //   else displayTurn();
-  // }
-
-  // function displayTurn() {
-  //   if (turn === 'X' ) {
-  //     playerO.innerHTML = 'Player O ';
-  //     playerX.innerHTML = 'Player X &diams; ';
-  //   }
-  //   else {
-  //     playerO.innerHTML = 'Player O &diams; ';
-  //     playerX.innerHTML = 'Player X ';
-  //   }
-  // }
-
-  // function gameOver(winner) {
-  //   if (winner === 'tie') {
-  //     alert("Tie!");
-  //   }
-  //   else {
-  //     highlightBorders(winner);
-  //     // winner[0] is 'X' or 'O'
-  //     alert(winner[0] + ' wins!');
-  //     // check for consecutive wins
-  //     checkConsecWins(winner[0]);
-  //   }
-  //   turn = 'game_over';
-  // }
-
-  // if any player scores three wins in a row, display a message
-  // function checkConsecWins(winner) {
-  //   if (winner === 'X') {
-  //     oConsecWins = 0;
-  //     if (++xConsecWins === 3)
-  //       playerX.innerHTML = 'Player X - <strong>HAT TRICK !!!</strong>';
-  //   }
-  //   else {
-  //     xConsecWins = 0;
-  //     if (++oConsecWins === 3);
-  //       playerO.innerHTML = 'Player O - <strong>HAT TRICK !!!</strong>';
-  //   }
-  // }
-
-  // getWinner will return a win in the form of a string denoting
-  // the winning player and winning squares, e.g. 'X048'
-  // if no win, return null
-  // function getWinner() {
-  //   if (winnerIs('X')) {
-  //     return 'X' + winnerIs('X');
-  //   }
-  //   else if (winnerIs('O')) {
-  //     return 'O' + winnerIs('O');
-  //   }
-  //   else
-  //     return null;
-  // }
-
-  // If a winning combination of squares exists, return the win
-  // in the form of a string denoting the winning squares, e.g. '048'
-  function winnerIs(player) {
-    return allThree(player, squareValue('0'), squareValue('1'), squareValue('2')) ||
-           allThree(player, squareValue('3'), squareValue('4'), squareValue('5')) ||
-           allThree(player, squareValue('6'), squareValue('7'), squareValue('8')) ||
-           allThree(player, squareValue('0'), squareValue('3'), squareValue('6')) ||
-           allThree(player, squareValue('1'), squareValue('4'), squareValue('7')) ||
-           allThree(player, squareValue('2'), squareValue('5'), squareValue('8')) ||
-           allThree(player, squareValue('0'), squareValue('4'), squareValue('8')) ||
-           allThree(player, squareValue('2'), squareValue('4'), squareValue('6'));
-  }
-
-  // returns the combo of winning rows, e.g. '345'
-  function allThree(player, squareOne, squareTwo, squareThree) {
-    if ((squareOne[0] === player) && (squareTwo[0] === player) && (squareThree[0] === player))
-      return squareOne[1] + squareTwo[1] + squareThree[1];
-  }
-
-  function squareValue(key) {
-    switch(key) {
-      // we want to persist the data denoting the location of the square on the board,
-      // as this will allow us to highlight winning squares in the event of a win
-      // example return values are 'X5', '02', '7' (space hasn't been played)
-      case '0': return squares[0].innerHTML + '0';
-      case '1': return squares[1].innerHTML + '1';
-      case '2': return squares[2].innerHTML + '2';
-      case '3': return squares[3].innerHTML + '3';
-      case '4': return squares[4].innerHTML + '4';
-      case '5': return squares[5].innerHTML + '5';
-      case '6': return squares[6].innerHTML + '6';
-      case '7': return squares[7].innerHTML + '7';
-      case '8': return squares[8].innerHTML + '8';
-      default : console.log('Something went wrong');
-    }
-  }
-
-  // winningSquares is a string in the form of 'X048'
-  function highlightBorders(winningSquares) {
-    squareBorders[parseInt(winningSquares[1])].setAttribute('class', 'win');
-    squareBorders[parseInt(winningSquares[2])].setAttribute('class', 'win');
-    squareBorders[parseInt(winningSquares[3])].setAttribute('class', 'win');
-  }
-
+  // listen for clicks on `reset` button to reset the board
+  reset.addEventListener('click', function () {
+    resetBoard();
+  });
 
 });
